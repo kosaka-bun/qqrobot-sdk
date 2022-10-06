03253a6318bb207998bd8f62978e28e08445c0f0
Kosaka Bun <kosaka-bun@qq.com>
2022-10-06 16:45:39 +0800

    Add qqrobot-demo

---
+++ qqrobot-demo/src/main/java/de/honoka/qqrobot/demo/QqRobotDemo.java
@@ -0,0 +1,12 @@
+package de.honoka.qqrobot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqRobotDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QqRobotDemo.class, args);
+    }
+}
