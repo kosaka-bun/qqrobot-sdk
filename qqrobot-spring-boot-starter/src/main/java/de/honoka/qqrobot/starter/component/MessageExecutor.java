ab5d5f4e080c4557d44ff994a20b96903404a4f6
Kosaka Bun <kosaka-bun@qq.com>
2022-11-06 20:25:51 +0800

    升级mirai-core版本

--- qqrobot-spring-boot-starter/src/main/java/de/honoka/qqrobot/starter/component/MessageExecutor.java
+++ qqrobot-spring-boot-starter/src/main/java/de/honoka/qqrobot/starter/component/MessageExecutor.java
@@ -14,6 +14,7 @@ import de.honoka.qqrobot.starter.component.session.SessionManager;
 import de.honoka.sdk.util.code.ActionUtils;
 import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -44,6 +45,7 @@ public class MessageExecutor {
     @Resource
     private SessionManager sessionManager;
 
+    @Lazy
     @Resource
     private ExceptionReporter reporter;
 
